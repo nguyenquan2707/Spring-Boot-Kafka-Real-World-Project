fb1f2d39c33cf2c37bc456a10dd92db3920f45a5
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-11 13:01:30 +0700

    first commit

---
+++ kafka-consumer/src/main/java/com/quan/SprintBootApplication.java
@@ -0,0 +1,12 @@
+package com.quan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootApplication.class);
+    }
+}
---
+++ kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/WikimediaChangesProducer.java
@@ -0,0 +1,43 @@
+package net.javaguides.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class WikimediaChangesProducer {
+
+//    @Value("${spring.kafka.topic.name}")
+    private String topicName = "wikimedia_recentchange";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() {
+        //
+        EventHandler eventHandler = new WikimediaChangeHandler(kafkaTemplate, topicName);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
